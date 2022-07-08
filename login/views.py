503140f6a59528a79ce77f778f50978c45f5bad0
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2022-07-08 15:35:20 -0300

    solving conflicts

--- login/views.py
+++ login/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
-
-def apresentacao(request):
-    return render(request, 'introducao.html')
-    
+ 
 def user_register(request):
     return render(request, 'login.html')
 
 def profile(request):
-<<<<<<< HEAD
-    return render(request,'Vcard.html')
-=======
     return render(request, 'vcard.html')
-
-
->>>>>>> c20096502cfd2b2c82ab574de5ac2bb0c58a0399
--- vcard/urls.py
+++ vcard/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-from login import views
 
 
 
