0edc0c4e5f478a79e9b020f9803247a3fe1e7ee5
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2022-07-26 15:41:54 -0300

    .

--- uservalidation/urls.py
+++ uservalidation/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import CreateUser
 from . import views
+
+
 urlpatterns = [
     path('user_login/',auth_views.LoginView.as_view(template_name='login/login.html'), name='user.login'),
     path('logout', views.logout_view, name='logout'),
